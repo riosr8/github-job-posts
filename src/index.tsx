ac3f6c908f41ff71a5e23031a1314a751b5e77a3
Raul Rios <raulrios1@live.com>
2020-07-09 23:05:59 -0500

    Add Chakra theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
