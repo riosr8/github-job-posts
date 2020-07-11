a4fc32b653276dd3b300f73596a95da9c17299d4
Raul Rios <raulrios1@live.com>
2020-07-11 09:35:41 -0500

    Update Job render function

--- src/components/job.tsx
+++ src/components/job.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Heading, PseudoBoxProps, Text, PseudoBox } from '@chakra-ui/core';
 
 export interface IJob {
     id: string;
@@ -13,8 +14,15 @@ export interface IJob {
     company_logo: string;
 }
 
-const Job: React.FC<IJob> = ({ title }) => {
-    return <div>{title}</div>;
+const Job: React.FC<IJob & PseudoBoxProps> = ({ title, company, ...rest }) => {
+    return (
+        <PseudoBox _hover={{ cursor: 'pointer', bg: 'gray.100' }} {...rest}>
+            <Box p={5} shadow='md' borderWidth='1px' borderRadius='6px'>
+                <Heading fontSize='xl'>{title}</Heading>
+                <Text fontSize='lg'>{company}</Text>
+            </Box>
+        </PseudoBox>
+    );
 }
 
 export default Job;
