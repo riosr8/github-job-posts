a024755e5a55ca33414b67abbc13585fe02c7420
Raul Rios <raulrios1@live.com>
2020-07-10 22:11:16 -0500

    move useInfiniteQuery hook to Jobs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
 import React from 'react';
 import { Grid, Flex, Box, Input } from '@chakra-ui/core';
-import { useInfiniteQuery } from 'react-query';
+import Jobs from './components/jobs';
 
 function App() {
 
   const [search, setSearch] = React.useState('');
 
-  const {
-    status,
-    data,
-    error,
-    isFetching,
-    isFetchingMore,
-    fetchMore,
-    canFetchMore,
-  } = useInfiniteQuery<any, any, any>(
-    ['jobs', { search }],
-    async (key: string, { search }, page: number = 0) => {
-      console.log(key, search, page)
-      const res = await fetch(`/positions.json?page=${page}&search=${search}`, {
-      });
-      const data = await res.json();
-      return { data, page: page + 1 };
-    },
-    {
-      getFetchMore: lastGroup => lastGroup.page,
-    }
-  );
-
   return (
     <Grid w={'90%'} gridGap='20px' m='0 auto' templateColumns='3fr' templateRows='auto' templateAreas={`'header' 'content'`}>
       <Flex gridArea='header' h='10vh' alignItems='center'>
-        <Input size='lg' borderWidth='3px' placeholder='Search...' />
+        <Input
+          size='lg'
+          borderWidth='3px'
+          placeholder='Search...'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
+        />
       </Flex>
-      <Box bg='#68d391' gridArea='content' h='100vh'>Content</Box>
+      <Box bg='#68d391' gridArea='content' h='100vh'>
+        <Jobs search={search} />
+      </Box>
     </Grid>
 
   );
---
+++ src/components/jobs.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Stack } from '@chakra-ui/core';
+import { useInfiniteQuery } from 'react-query';
+
+interface JobsProps {
+    search: string;
+}
+
+const Jobs: React.FC<JobsProps> = ({ search }) => {
+    const {
+        status,
+        data,
+        error,
+        isFetching,
+        isFetchingMore,
+        fetchMore,
+        canFetchMore,
+    } = useInfiniteQuery<any, any, any>(
+        ['jobs', { search }],
+        async (key: string, { search }, page: number = 0) => {
+            console.log(key, search, page)
+            const res = await fetch(`/positions.json?page=${page}&search=${search}`, {
+            });
+            const data = await res.json();
+            return { data, page: page + 1 };
+        },
+        {
+            getFetchMore: lastGroup => lastGroup.page,
+        }
+    );
+
+    return <div>jobs</div>;
+}
+
+export default Jobs;
+
+
