0004002815bd1cd22c5b5a8c29879d45c93e86bf
Raul Rios <raulrios1@live.com>
2020-07-11 19:14:44 -0500

    add useQuery hook to JobDetails

--- src/components/job-details.tsx
+++ src/components/job-details.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button } from "@chakra-ui/core";
+import { useQuery } from 'react-query';
 
 interface JobDetailsProps {
     id: string;
@@ -8,6 +9,22 @@ interface JobDetailsProps {
 }
 
 const JobDetails: React.FC<JobDetailsProps> = ({ id, onClose, isOpen }) => {
+
+    const {
+        isLoading, isError, data
+    } = useQuery(
+        ['job', id],
+        async (key: string, id: string) => {
+            try {
+                const res = await fetch(`/positions/${id}.json?markdown=true`);
+                return await res.json();
+            } catch (error) {
+                throw error
+            }
+        }
+    )
+
+
     return (
         <Modal preserveScrollBarGap isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
