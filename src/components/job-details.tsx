19de5bedeb8397264afc2dc98e39a4958f27ceba
Raul Rios <raulrios1@live.com>
2020-07-11 20:31:02 -0500

    add content to modal

--- src/components/job-details.tsx
+++ src/components/job-details.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button } from "@chakra-ui/core";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Text, Flex, Box } from "@chakra-ui/core";
 import { useQuery } from 'react-query';
+import { IJob } from './job';
+import ReactMarkdown from 'react-markdown';
 
 interface JobDetailsProps {
     id: string;
@@ -14,7 +16,7 @@ const JobDetails: React.FC<JobDetailsProps> = ({ id, onClose, isOpen }) => {
         isLoading, isError, data
     } = useQuery(
         ['job', id],
-        async (key: string, id: string) => {
+        async (key: string, id: string): Promise<IJob> => {
             try {
                 const res = await fetch(`/positions/${id}.json?markdown=true`);
                 return await res.json();
@@ -26,21 +28,44 @@ const JobDetails: React.FC<JobDetailsProps> = ({ id, onClose, isOpen }) => {
 
 
     return (
-        <Modal preserveScrollBarGap isOpen={isOpen} onClose={onClose}>
+        <Modal preserveScrollBarGap isOpen={isOpen} onClose={onClose} blockScrollOnMount>
             <ModalOverlay />
             <ModalContent>
-                <ModalHeader>Modal Title</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    {id}
-                </ModalBody>
+                {
+                    isLoading ?
+                        'Loading...' :
+                        isError ?
+                            'Error' :
+                            (
+                                <>
+                                    <ModalHeader>
+                                        {data?.title}
+                                        <Flex justifyContent='space-between'>
+                                            <Text fontWeight='normal' fontSize='sm'>{data?.company}</Text>
+                                            <Text fontWeight='normal' fontSize='sm'>{data?.location}/{data?.type}</Text>
+                                        </Flex>
+                                    </ModalHeader>
+                                    <ModalCloseButton />
+                                    <ModalBody>
+                                        <Box>
+                                            <Text fontWeight='bold' textDecor='underline'>How to apply</Text>
+                                            <ReactMarkdown source={data?.how_to_apply} />
+                                        </Box>
+                                        <Box>
+                                            <Text fontWeight='bold' textDecor='underline'>Description</Text>
+                                            <ReactMarkdown source={data?.description} />
+                                        </Box>
 
-                <ModalFooter>
-                    <Button variantColor="blue" mr={3} onClick={onClose}>
-                        Close
-                    </Button>
-                    <Button variant="ghost">Secondary Action</Button>
-                </ModalFooter>
+                                    </ModalBody>
+                                    <ModalFooter>
+                                        <Button variantColor="blue" mr={3} onClick={onClose}>
+                                            Close
+                                        </Button>
+                                    </ModalFooter>
+                                </>
+                            )
+                }
+
             </ModalContent>
         </Modal>
     );
